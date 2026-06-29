2e4f92fb57edfd31647229af34715bc0272d3a78
fries-git <fries_09@hotmail.com>
2026-06-28 20:07:03 -0400

    more types, ignores ogg files, fixes.

--- main.py
+++ main.py
@@ -1,27 +1,68 @@
 import tkinter as tk
 from tkinter import filedialog
+
+from ffprobe import FFProbe as prb
 import ffmpeg
 from pathlib import Path
 
+validtypes = [
+    ".mp3",
+    ".wav",
+    ".flac",
+    ".aac",
+    ".m4a",
+    ".wma",
+    ".aiff",
+    ".ogg",
+    ".aif",
+    ".opus",
+    ".oga",
+    ".ac3",
+    ".eac3",
+    ".amr",
+    ".ape",
+    ".alac",
+    ".au",
+    ".caf",
+    ".dts",
+    ".mka",
+    ".mp2",
+    ".mp1",
+    ".ra",
+    ".rm",
+    ".tta",
+    ".voc",
+    ".wv",
+    ".8svx",
+    ".snd"
+]
+
 def process(file):
     output = Path(file).with_suffix(".ogg")
-    print(f"Processing file: {file}")
+    if Path(file).suffix in validtypes: 
+        if not Path(file).suffix == ".ogg":
+            try:
+                print(f"Processing file: {Path(file).name}")
+                probe = prb(file)
+                for stream in probe.streams:
+                    print(f"Duration: {stream.duration}")
+                (
+                    ffmpeg
+                    .input(str(file))
+                    .output(str(output), format="ogg", acodec="libvorbis")
+                    .overwrite_output()
+                    .run(quiet=True)
+                )
 
-    try:
-        (
-            ffmpeg
-            .input(str(file))
-            .output(str(output), format="ogg", acodec="libvorbis")
-            .overwrite_output()
-            .run(quiet=True)
-        )
+            except ffmpeg.Error as e:
+                print(f"Failed with FFMPEG error: {e}")
 
-    except ffmpeg.Error as e:
-        print(f"Failed with FFMPEG error: {e}")
-
-    except Exception as e:
-        print(f"Failed with misc. error: {e}") 
-
+            except Exception as e:
+                print(f"Failed with misc. error: {e}")
+        else:
+            print(f"File: {Path(file).name} is already ogg!")
+    else:
+        print(f"File: {Path(file).name} is not of supported type.")
 print("Welcome to mp32ogg, I can't think of a clever acronym or name so eat my short.")
 
 root = tk.Tk()
